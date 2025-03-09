9614ec321f5f5a3639103926283cc4b0952ee660
andreavelino02 <andre.avelino@ilegra.com>
2025-03-08 21:26:37 -0400

    Ajuste final de rotas para Azure

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import Dashboard from "../dashboard/dashboard";
 import BillingCycle from "../billingCycle/billingCycle";
 
 export default (props) => (
   <div className="content-wrapper">
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/billingCycles" component={BillingCycle} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/billingCycles" component={BillingCycle} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
   </div>
 );
