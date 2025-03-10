87d71370cb8e7082207f3b5917560aceee03ddc2
andreavelino02 <andre.avelino@ilegra.com>
2025-03-10 14:31:29 -0400

    ajustes no import

--- src/billingCycle/actions/billingCycleActions.js
+++ src/billingCycle/actions/billingCycleActions.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { toastr } from "react-redux-toastr";
 import { reset as resetForm, initialize } from "redux-form";
 import { showTabs, selectTab } from "../../common/tab/actions/tabActions";
-import { API_URL } from "../../consts"; // Ajuste o caminho conforme necessário
+import  config  from "../../consts"; // Ajuste o caminho conforme necessário
 
-const BASE_URL = API_URL;
+const BASE_URL = config.API_URL;
 const INITIAL_VALUES = {credits: [{}], debts: [{}]};
 
 export function getList() {
